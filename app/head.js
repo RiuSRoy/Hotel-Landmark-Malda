40a26d6b4263f79c65e19cc4586db619f10519e2
RiuSRoy <iamash20@gmail.com>
2023-12-15 01:14:13 +0530

    Landing page

--- app/head.js
+++ app/head.js
@@ -1,10 +1,15 @@
+import Script from "next/script";
+
 export default function Head() {
   return (
     <>
+    <Script async src="https://www.googletagmanager.com/gtag/js?id=G-20J1DVSCYF"/>
       <title>Hotel Landmark Malda: Hospitality at its best | Book Your Perfect Stay</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Best Hotel in Malda at affordable rates with in-house retro and banquet cum conference hall" />
       <link rel="icon" href="/favicon.ico" />
+
+      
     </>
   )
 }
