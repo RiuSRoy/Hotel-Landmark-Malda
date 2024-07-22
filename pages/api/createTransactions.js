a783104195ecf10e6db3bf8e897ab7d2db312468
RiuSRoy <iamash20@gmail.com>
2024-07-22 11:32:34 +0530

    api changes for Fanclahs 3rd party

---
+++ pages/api/createTransactions.js
@@ -0,0 +1,56 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default async function handler(req, res) {
+    try {
+
+        const x = req.body;
+        // return res.status(200).json({ 
+        //     status: "error",
+        //     errorCode: "er_s5",
+        //     "errorMessage": "UNEXPECTED_ERROR!",
+        // })
+        
+        console.log(x);
+        const r = Math.random();
+        if (r < 0.8) {
+    
+            res.status(200).json({
+                status: "success",
+                data: {
+                    updatedUserBalance: 1000,
+                    "txns": x.txns.map(txn => {
+                        return {
+                            referenceId: txn.referenceId,
+                            merchantTransactionId: txn.referenceId + '_' + randomIntFromInterval(0, 1000)
+                        }
+                    })
+                }
+            });
+        } else {
+            res.status(200).json({ 
+                status: "error",
+                errorCode: "er_w1",
+                "errorMessage": "User Wallet is not active!",
+            })
+        } 
+    } catch (e) {
+            res.status(200).json({ 
+                status: "error",
+                errorCode: "er_s5",
+                "errorMessage": "UNEXPECTED_ERROR!",
+                message: e.message
+            })
+    }
+  }
+  
+  /**
+   * 
+   status: "error",
+        errorCode: "er_w3",
+        "errorMessage": "Balance is insufficient!",
+        */
+
+
+        export function randomIntFromInterval(min, max) { // min and max included 
+            return Math.floor(Math.random() * (max - min + 1) + min)
+        }
---
+++ pages/api/getTransactions.js
@@ -0,0 +1,43 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import { randomIntFromInterval } from "./createTransactions";
+
+export default function handler(req, res) {
+    const x = req.body;
+    console.log(x);
+    const transactions = x.referenceIds.map(txn => {
+        return {
+            referenceId: txn,
+            transactionId: txn + '_' + randomIntFromInterval(1, 1000),
+            status: 'completed',
+        }
+    })
+    const r = Math.random();
+    if (r < 0.8) {
+        res.status(200).json({
+            status: "success",
+            data: {
+                transactions,
+            }
+        })
+    } else if (r > 0.95) {
+        res.status(200).json({
+            status: "error",
+            errorCode: "er_s5",
+            "errorMessage": "Unexpected error!",
+        })
+    } else {
+        res.status(200).json({
+            status: "error",
+            errorCode: "er_w4",
+            "errorMessage": "Transaction not found!",
+        })
+    }
+  }
+  
+  /**
+   * 
+   status: "error",
+        errorCode: "er_w3",
+        "errorMessage": "Balance is insufficient!",
+        */
